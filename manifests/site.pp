b26afa25e1630df11cdafac0a853654fa5e6cf46
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-09-06 17:14:39 +0300

    Take out firewall class from sites

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,19 +7,6 @@ node 'puppetclient.example.com' {
   include mysql
   include php
 
-  resources { 'firewall':
-    purge => true,
-  }
-
-  Firewall {
-    before        => Class['firewall::post'],
-    require       => Class['firewall::pre'],
-  }
-
-  class { ['firewall::pre', 'firewall::post']: }
-
-  }
-
 node 'puppetcentos.example.com' {
   $adminemail = 'puppetmaster@example.com'
   $servername = 'puppetmaster.example.com'
@@ -28,16 +15,3 @@ node 'puppetcentos.example.com' {
   include apache::vhosts
   include mysql
   include php
-
-  resources { 'firewall':
-    purge => true,
-  }
-
-  Firewall {
-    before        => Class['firewall::post'],
-    require       => Class['firewall::pre'],
-  }
-
-  class { ['firewall::pre', 'firewall::post']: }
-
-  }
