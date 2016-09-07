feeb588a0a88c93009b5ca92a477dae68dd78f6e
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-09-07 19:56:17 +0300

    Changed mysql to mariadb on RHEL/CentOS

--- modules/mysql/manifests/params.pp
+++ modules/mysql/manifests/params.pp
@@ -1,20 +1,21 @@
 class mysql::params {
 
   $mysqlpassword = "Passw0rd"
-  $mysql_name     = 'mysql-server'
 
   if $::osfamily == 'RedHat' {
-    $mysql_service  = 'mysqld'
-    $mysql_conf       = '/etc/my.cnf'
-    $mysql_src     = 'puppet:///modules/mysql/my-centos.cnf'
+    $mysql_name     = 'mariadb-server'
+    $mysql_service  = 'mariadb'
+    $mysql_conf     = '/etc/my.cnf'
+    $mysql_src      = 'puppet:///modules/mysql/my-centos.cnf'
     $init_cmd       = '/bin/true'
     $mysqlpwd_cmd   = '/usr/bin/mysqladmin -u root password $mysqlpassword || /bin/true'
   }
 
   elsif $::osfamily == 'Debian' {
+    $mysql_name     = 'mysql-server'
     $mysql_service  = 'mysql'
-    $mysql_conf       = '/etc/mysql/my.cnf'
-    $mysql_src     = 'puppet:///modules/mysql/my-ubuntu.cnf'
+    $mysql_conf     = '/etc/mysql/my.cnf'
+    $mysql_src      = 'puppet:///modules/mysql/my-ubuntu.cnf'
     $init_cmd       = '/sbin/initctl reload-configuration'
     $mysqlpwd_cmd   = '/usr/bin/mysqladmin -u root password $mysqlpassword || /bin/true'
   }
