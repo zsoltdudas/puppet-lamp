d7ff0ac24df27e8714737908734eb0b61604d253
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-09-19 16:44:00 +0300

    Test nodes cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,39 +1,7 @@
+#Please fill this out with the nodes fqdn and email address
 node 'puppetclient.example.com' {
-  $adminemail = 'puppetmaster@example.com'
-  $servername = 'puppetmaster.example.com'
-
-  include apache
-  include apache::vhosts
-  include mysql
-  include php
-  include firewall
-}
-
-node 'puppetcentos.example.com' {
-  $adminemail = 'puppetmaster@example.com'
-  $servername = 'puppetmaster.example.com'
-
-  include apache
-  include apache::vhosts
-  include mysql
-  include php
-  include firewall
-}
-
-node 'puppetnew.example.com' {
-  $adminemail = 'puppetmaster@example.com'
-  $servername = 'puppetmaster.example.com'
-
-  include apache
-  include apache::vhosts
-  include mysql
-  include php
-  include firewall
-}
-
-node 'centos68.example.com' {
-  $adminemail = 'puppetmaster@example.com'
-  $servername = 'puppetmaster.example.com'
+  $adminemail = 'puppetclient@example.com'
+  $servername = 'puppetclient.example.com'
 
   include apache
   include apache::vhosts
